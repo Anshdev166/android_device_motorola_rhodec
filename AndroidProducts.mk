e5f1dc5ed7645259e9a77f57e05849f67c69aefc
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-10-02 07:51:37 +0200

    rhodec: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: moto g62 5G Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_rhodec.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_rhodec-user \
+    omni_rhodec-userdebug \
+    omni_rhodec-eng
