dd6ae6c99087ea41fff95234af8de9f7870d252f
Anshdev166 <fightergun8606@gmail.com>
2025-10-01 22:58:20 -0700

    Update and rename omni_rhodec.mk to twrp_rhodec.mk

--- twrp_rhodec.mk
+++ twrp_rhodec.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from rhodec device
 $(call inherit-product, device/motorola/rhodec/device.mk)
 
 PRODUCT_DEVICE := rhodec
-PRODUCT_NAME := omni_rhodec
+PRODUCT_NAME := twrp_rhodec
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g62 5G
 PRODUCT_MANUFACTURER := motorola
